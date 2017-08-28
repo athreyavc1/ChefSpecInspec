7b73fc994f7c8c3af9f316624a04064f0cbeee29
user <user@localhost.localdomain>
2017-08-28 08:08:42 +0100

    chefSpec tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, Kurt Wallander, All Rights Reserved.
+#
+package 'httpd' do 
+  action :install 
+end 
+
+package 'net-tools' do 
+  action :install 
+end 
+
+service 'httpd' do 
+ action [:enable, :start]
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,5 +18,15 @@ describe 'lcd_web::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+    packages = ['net-tools', 'httpd']
+    packages.each do | line | 
+      it "installs #{line} package" do 
+        expect(chef_run).to install_package(line)
+      end
+    end 
+    it 'starts and enables service' do 
+      expect(chef_run).to enable_service('httpd')
+      expect(chef_run).to start_service('httpd')
+    end
   end
 end
